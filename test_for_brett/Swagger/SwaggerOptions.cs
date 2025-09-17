4e1e925be8db4e88271b1892a45c477b7ac5b8e4
Terraform User <brett.lewerke@insight.com>
2025-09-17 13:48:45 -0700

    Insight GitHub Repository Template

---
+++ test_for_brett/Swagger/SwaggerOptions.cs
@@ -0,0 +1,10 @@
+namespace test_for_brett.Swagger
+
+{
+    public class SwaggerOptions
+    {
+        public string? ClientId { get; set; }
+        public string? AuthorizationUrl { get; set; }
+        public string? AuthScope { get; set; }
+    }
+}
